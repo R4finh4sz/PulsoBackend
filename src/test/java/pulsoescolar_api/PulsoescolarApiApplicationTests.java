d1bb06fc52f73aaffcab411c0a66e67866859719
R4finh4sz <rafael.souza6657@gmail.com>
2026-09-06 17:35:01 -0300

    Feat: First Commit

---
+++ src/test/java/pulsoescolar_api/PulsoescolarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pulsoescolar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsoescolarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
