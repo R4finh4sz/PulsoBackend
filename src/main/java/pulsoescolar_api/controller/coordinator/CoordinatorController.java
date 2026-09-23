4904883c0850d9e13bf9437cca25f4eedd4f73d5
R4finh4sz <rafael.souza6657@gmail.com>
2026-09-23 19:56:04 -0300

    Fix: Remove unused import

--- src/main/java/pulsoescolar_api/controller/coordinator/CoordinatorController.java
+++ src/main/java/pulsoescolar_api/controller/coordinator/CoordinatorController.java
@@ -1,6 +1,5 @@
 package pulsoescolar_api.controller.coordinator;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
 import lombok.RequiredArgsConstructor;
