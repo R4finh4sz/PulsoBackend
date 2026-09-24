e16cf86c7ae85dfe910c7912032e737df2b27f26
R4finh4sz <rafael.souza6657@gmail.com>
2026-09-24 00:11:36 -0300

    Fix: Error in entity

--- src/main/java/pulsoescolar_api/entity/user/SchoolUser.java
+++ src/main/java/pulsoescolar_api/entity/user/SchoolUser.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Entity @Table(name="school_users")
 @Getter @Setter @NoArgsConstructor
 public class SchoolUser {
+ private java.time.Instant twoFactorResendAvailableAt;
  @ManyToOne(fetch=FetchType.LAZY) @JoinColumn(name="school_id") private School school;
  @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
  @Column(nullable=false, length=200) private String fullName;
